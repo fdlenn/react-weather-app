e20a9f38c86ba562b71772b49595a75f3c8c5101
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-01 13:22:28 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import "./App.css";
 
 export default function App() {
   return <div className="App">
-    Weather App
+    <div className="Container">
+    <h1>Weather App</h1>
     <footer>
       This porject was coded by Laura Fenn and is{" "}
     <a href="https://github.com/fdlenn/react-weather-app.git" target="_blank">open-sourced</a>
     </footer>
+    </div>
     </div>
 }
 
