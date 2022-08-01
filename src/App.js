3fcdc7b2edaa01b0a5862449e06eb91e33b1c3dc
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-01 12:27:26 +0100

    Added footer with GitHub URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>
+  return <div className="App">
+    Weather App
+    <footer>
+      This porject was coded by Laura Fenn and is{" "}
+    <a href="https://github.com/fdlenn/react-weather-app.git" target="_blank">open-sourced</a>
+    </footer>
+    </div>
 }
 
 
