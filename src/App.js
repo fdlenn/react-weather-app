333e01bf596e017ecaa3b1be057715fefa6f3992
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-01 13:49:56 +0100

    Added basic HTML and search function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Weather from "./Weather.js";
 import "./App.css";
 
 export default function App() {
   return <div className="App">
     <div className="Container">
     <h1>Weather App</h1>
+    <Weather />
     <footer>
       This porject was coded by Laura Fenn and is{" "}
     <a href="https://github.com/fdlenn/react-weather-app.git" target="_blank">open-sourced</a>
