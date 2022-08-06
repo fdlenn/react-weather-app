557fc9bd9b5685f7e712bf2edbd7605a0e4bd0c5
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-06 15:39:29 +0100

    Added CSS

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ export default function App() {
     <h1>Weather App</h1>
     <Weather />
     <footer>
-      This porject was coded by Laura Fenn and is{" "}
-    <a href="https://github.com/fdlenn/react-weather-app.git" target="_blank">open-sourced</a>
+      This Weather App was coded by Laura Fenn and is{" "}
+    <a href="https://github.com/fdlenn/react-weather-app.git" target="_blank">open-sourced</a>.
     </footer>
     </div>
     </div>
--- src/Weather.js
+++ src/Weather.js
@@ -6,10 +6,10 @@ return <div className="Weather">
     <form>
         <div className="row">
             <div className="col-9">
-        <input type="search" placeholder="Search for a city..." className="form-control" />
+        <input type="search" placeholder="Search for a city..." className="form-control" autoFocus="on" />
         </div>
         <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary"/>
+        <input type="submit" value="Search" className="btn btn-primary w-100"/>
         </div>
         </div>
     </form>
@@ -18,10 +18,18 @@ return <div className="Weather">
     <li>Monday 07:00</li>
     <li>Mostly Cloudy</li>
 </ul>
-<div className="row">
+<div className="row mt-3">
     <div className="col-6">
-        <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" alt="Mostly Cloudy"/>
-        6°C
+        <div className="clearfix">
+            <img src="https://ssl.gstatic.com/onebox/weather/64/cloudy.png" 
+            alt="Mostly Cloudy" 
+            className="float-left"
+            />
+          <div className="float-left">
+                <span className="temperature">6</span>
+                <span className="unit">°C</span>
+                </div>
+                </div>
     </div>
     <div className="col-6">
         <ul>
