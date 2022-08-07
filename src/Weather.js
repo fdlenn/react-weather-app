e566e4bd4ccde0bc52990d2b6a8d850df47fbc18
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-07 21:06:02 +0100

    Animated icon (changed icon pack)

--- src/Weather.js
+++ src/Weather.js
@@ -37,18 +37,28 @@ setCity(event.target.value);
 if (weatherData.ready) {
     return ( 
     <div className="Weather">
-    <form onSubmit={handleSubmit}>
-        <div className="row">
-            <div className="col-9">
-        <input type="search" placeholder="Search for a city..." className="form-control" autoFocus="on" onChange={handleCityChange} />
-        </div>
-        <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary w-100"/>
-        </div>
-        </div>
-    </form>
-    <WeatherInfo data={weatherData} />
-</div>
+        <form onSubmit={handleSubmit}>
+            <div className="row">
+                <div className="col-9">
+                    <input 
+                    type="search" 
+                    placeholder="Search for a city..." 
+                    className="form-control" 
+                    autoFocus="on" 
+                    onChange={handleCityChange} 
+                    />
+                </div>
+                  <div className="col-3">
+                    <input 
+                    type="submit" 
+                    value="Search" 
+                    className="btn btn-primary w-100"
+                    />
+                  </div>
+            </div>
+        </form>
+        <WeatherInfo data={weatherData} />
+    </div>
     );
 } else {
     search();
