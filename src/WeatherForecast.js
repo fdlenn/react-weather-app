171a90d280315cc493279e3e5d46f96bdece14ae
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-21 14:50:40 +0100

    Added WeatherForecastDay

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,63 +1,35 @@
-import React from "react";
-import WeatherIcon from "./WeatherIcon";
+import React, { useState } from "react";
 import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
-    function handleResponse(response) {
-console.log(response.data);
-    }
+    let [loaded, setLoaded] = useState(false);
+    let [forecast, setForecast] = useState(null);
+
+function handleResponse(response) {
+setForecast(response.data.daily);
+setLoaded(true);
+}
+
+if (loaded) {
+    return (
+        <div className="WeatherForecast"> 
+            <div className="row">
+                <div className="col">
+                    <WeatherForecastDay data={forecast[0]} />
+                </div>
+            </div>
+        </div>
+        );
+    } else {
     let apiKey = "2f6c0570b02743f6b440bec23aa5a698";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
     let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
-axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleResponse);
 
-return (
-<div className="WeatherForecast"> 
-    <div className="row">
-        <div className="col">
-            <div className="WeatherForecast-day">Thur</div> 
-            <WeatherIcon code="01d" size={36} />
-                <div className="WeatherForecast-temperatures"> 
-                <span className="WeatherForecast-temp-max">19°</span>
-                <span className="WeatherForecast-temp-min">10°</span>
-            </div>  
-        </div>
-        <div className="col">
-            <div className="WeatherForecast-day">Thur</div> 
-            <WeatherIcon code="01d" size={36} />
-                <div className="WeatherForecast-temperatures"> 
-                <span className="WeatherForecast-temp-max">19°</span>
-                <span className="WeatherForecast-temp-min">10°</span>
-            </div>  
-        </div>
-        <div className="col">
-            <div className="WeatherForecast-day">Thur</div> 
-            <WeatherIcon code="01d" size={36} />
-                <div className="WeatherForecast-temperatures"> 
-                <span className="WeatherForecast-temp-max">19°</span>
-                <span className="WeatherForecast-temp-min">10°</span>
-            </div>  
-        </div>
-        <div className="col">
-            <div className="WeatherForecast-day">Thur</div> 
-            <WeatherIcon code="01d" size={36} />
-                <div className="WeatherForecast-temperatures"> 
-                <span className="WeatherForecast-temp-max">19°</span>
-                <span className="WeatherForecast-temp-min">10°</span>
-            </div>  
-        </div>
-        <div className="col">
-            <div className="WeatherForecast-day">Thur</div> 
-            <WeatherIcon code="01d" size={36} />
-                <div className="WeatherForecast-temperatures"> 
-                <span className="WeatherForecast-temp-max">19°</span>
-                <span className="WeatherForecast-temp-min">10°</span>
-            </div>  
-        </div>
-    </div>
-</div>
-);
+    return null;
+    }
 }
