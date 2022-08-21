e1eb6ed82fbfbf9c8a5e11c0fdc8be9c1fedfa0f
fdlenn <101424376+fdlenn@users.noreply.github.com>
2022-08-21 21:21:23 +0100

    corrected error in apiUrl

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -26,7 +26,7 @@ if (loaded) {
     let apiKey = "2f6c0570b02743f6b440bec23aa5a698";
     let longitude = props.coordinates.lon;
     let latitude = props.coordinates.lat;
-    let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
 
     axios.get(apiUrl).then(handleResponse);
 
